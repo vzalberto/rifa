405c57844fc312ad97cc5db2119d449874830069
Alberto <vzalberto@msn.com>
2019-01-31 13:22:34 -0600

    First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './luck.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      ballot:[],
+      winner: null
+    }
+  }
+
+  componentWillMount(){
+    this.initBallot();
+  }
+
+  componentWillUpdate(){
+    if (this.state.ballot.length === 1){
+      const winner = this.state.ballot[0]
+      console.log(winner)
+      // this.setState( state => {
+      //   return {winner : winner}
+      // })
+      // console.log(this.state.winner) 
+    }
+  }
+
+  initBallot(){
+    this.setState( state => {
+      return {ballot: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+    })
+  }
+
+  handleButtonClick = () =>{
+    console.log('click')
+    let newBallot = this.state.ballot;
+    newBallot.pop();
+    this.setState({ballot: newBallot})
+  }
+
   render() {
+    let lista = null;
+    if( this.state.ballot ){
+      lista = (
+        <ul>
+          {this.state.ballot.map((el)=>{
+            return <li>{el}</li>
+          })}
+        </ul>
+      );
+    }
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" onClick={this.handleButtonClick}/>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Haga click en el ícono para sacar números
           </p>
+
+          {lista}
+
           <a
             className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
+            href="/"
             rel="noopener noreferrer"
           >
-            Learn React
+            Reiniciar
           </a>
         </header>
       </div>
