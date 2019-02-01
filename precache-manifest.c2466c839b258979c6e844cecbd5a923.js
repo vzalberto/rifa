64c4e3767f261386a24d99a8636fb8970c476082
Alberto <vzalberto@msn.com>
2019-02-01 10:01:12 -0600

    add build for github pages

---
+++ precache-manifest.c2466c839b258979c6e844cecbd5a923.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbb62caf63d183f83d675530f2c5b531",
+    "url": "/static/media/raffle.bbb62caf.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e350195a8017c5f9ce9b",
+    "url": "/static/js/main.e350195a.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "e350195a8017c5f9ce9b",
+    "url": "/static/css/main.1c30cb59.chunk.css"
+  },
+  {
+    "revision": "10125d08a017bce69c862f49b794fdf9",
+    "url": "/index.html"
+  }
+];
